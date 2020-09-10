f82c621138b48ced3c31d2765ffabdf07c2ee318
nhathuy7996 <nhat.huy.7996@gmail.com>
2020-09-10 11:41:31 +0700

    path creator

---
+++ PathCreator/PathCreator.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+[AddComponentMenu("HuyTools/PathCreator")]
+public class PathCreator : MonoBehaviour
+{
+    [SerializeField]
+    bool isLoop = false;
+    [SerializeField]
+    public List<Vector3> List_Points;
+
+    protected Vector3 _originalTransformPosition;
+    public Vector3 originalTransformPosition => _originalTransformPosition;
+
+    protected bool _originalTransformPositionStatus = false;
+    public bool originalTransformPositionStatus => _originalTransformPositionStatus;
+
+
+    protected virtual void Start()
+    {
+        Initialization();
+    }
+
+    protected virtual void Initialization()
+    {
+
+        if (List_Points == null || List_Points.Count < 1)
+        {
+            return;
+        }
+
+        if (!_originalTransformPositionStatus)
+        {
+            _originalTransformPositionStatus = true;
+            _originalTransformPosition = transform.position;
+        }
+        transform.position = _originalTransformPosition;
+    }
+
+#if UNITY_EDITOR
+    protected virtual void OnDrawGizmos()
+    {
+        if (List_Points == null)
+        {
+            return;
+        }
+
+        if (List_Points.Count == 0)
+        {
+            return;
+        }
+
+        if (_originalTransformPositionStatus == false)
+        {
+            _originalTransformPosition = transform.position;
+            _originalTransformPositionStatus = true;
+        }
+        if (transform.hasChanged)
+        {
+            _originalTransformPosition = transform.position;
+        }
+
+        for (int i = 0; i < List_Points.Count; i++)
+        {
+
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(_originalTransformPosition + List_Points[i], 0.2f);
+
+            if ((i + 1) < List_Points.Count)
+            {
+                Gizmos.color = Color.white;
+                Gizmos.DrawLine(_originalTransformPosition + List_Points[i], _originalTransformPosition + List_Points[i + 1]);
+            }
+        }
+
+        if (isLoop)
+        {
+            Gizmos.color = Color.white;
+            Gizmos.DrawLine(_originalTransformPosition + List_Points[List_Points.Count - 1], _originalTransformPosition + List_Points[0]);
+        }
+    }
+#endif
+
+}
---
+++ PathCreator/PathEditor.cs
@@ -0,0 +1,50 @@
+﻿#if UNITY_EDITOR
+
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(PathCreator), true)]
+[InitializeOnLoad]
+public class PathEditor : Editor
+{
+    public PathCreator pathTarget
+    {
+        get
+        {
+            return (PathCreator)target;
+        }
+    }
+
+    protected virtual void OnSceneGUI()
+    {
+        Handles.color = Color.green;
+        PathCreator t = (target as PathCreator);
+
+        if (t.originalTransformPositionStatus == false)
+        {
+            return;
+        }
+
+        for (int i = 0; i < t.List_Points.Count; i++)
+        {
+            EditorGUI.BeginChangeCheck();
+
+            Vector3 oldPoint = t.originalTransformPosition + t.List_Points[i];
+            GUIStyle style = new GUIStyle();
+
+            style.normal.textColor = Color.yellow;
+            Handles.Label(t.originalTransformPosition + t.List_Points[i] + (Vector3.down * 0.4f) + (Vector3.right * 0.4f), "" + i, style);
+
+            Vector3 newPoint = Handles.FreeMoveHandle(oldPoint, Quaternion.identity, .5f, new Vector3(.25f, .25f, .25f), Handles.CircleHandleCap);
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(target, "Free Move Handle");
+                t.List_Points[i] = newPoint - t.originalTransformPosition;
+            }
+        }
+    }
+}
+
+#endif

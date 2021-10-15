5325e3793fec2141d350c0e35b4c2925b60b0264
nhathuy7996 <nhat.huy.7996>
2021-10-15 15:19:50 +0700

    add invert UI mask

---
+++ InvertMaskUI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering;
+
+public class InvertMaskUI : Image
+{
+    public override Material materialForRendering
+    {
+
+
+        get
+        {
+            Material material = new Material(base.materialForRendering);
+            material.SetInt("_StencilComp", (int)CompareFunction.NotEqual);
+            return material;
+        }
+
+
+    }
+}

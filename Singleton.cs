36654264648eca9f39df4ac19dd861a2169c8269
nhathuy7996 <nhat.huy.7996@gmail.com>
2023-05-31 15:19:22 +0700

    fix singleton

--- Singleton.cs
+++ Singleton.cs
@@ -2,35 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+namespace DVAH
 {
-    private static T instant = null;
-    public static T Instant{
-        get{
-            if(instant ==null){
-                T[] Ts = FindObjectsOfType<T>();
-                if(Ts.Length > 0)
-                    instant = Ts[0];
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T instant = null;
+        public static T Instant => instant;
+         
+        void Awake()
+        {
+            if (instant == null)
+            {
+                instant = this.GetComponent<T>();
+            } else if (!instant.GetInstanceID().Equals(this.GetInstanceID())) {
+                Debug.LogErrorFormat("Duplicated singleton between {0} - {1}!, destroy {1}",instant.gameObject.name,this.gameObject.name);
             }
-
-            return instant;
+ 
         }
-    }
-
-    void Awake(){
-        T[] Ts = FindObjectsOfType<T>();
-        if(Ts.Length > 1)
-            Destroy(this);
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
+

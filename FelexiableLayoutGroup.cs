9239ca6094790e49a2994adce915101d7afbeeb1
nhathuy7996 <nhat.huy.7996@gmail.com>
2024-10-03 15:49:15 +0700

    Update FelexiableLayoutGroup.cs

--- FelexiableLayoutGroup.cs
+++ FelexiableLayoutGroup.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -37,21 +37,31 @@ public class FelexiableLayoutGroup : LayoutGroup
         float parentWidth = rectTransform.rect.width;
         float parentHeight = rectTransform.rect.height;
 
-        float cellWidth = parentWidth / (float)columns  - ((spacing.x / (float)columns) * 2) -  (padding.left / (float) columns) - (padding.right / (float) columns);
-        float cellHeight = parentHeight / (float)rows - ((spacing.y / (float)rows) * 2) - (padding.top / (float)rows) - (padding.bottom / (float)rows);
+        float cellWidth = (parentWidth -  (spacing.x * (columns - 1)) - padding.left - padding.right) / (float)columns ;
+        float cellHeight = (parentHeight - (spacing.y * (rows -1) - padding.top - padding.bottom) ) / (float)rows ;
 
         cellSize.x = FitX ? cellWidth : cellSize.x;
         cellSize.y = FitY ? cellHeight : cellSize.y;
 
         int columnCount = 0, rowCount = 0;
+        bool isCenter = (int)childAlignment == 1 || (int)childAlignment == 4 || (int)childAlignment == 7;
         for (int i = 0; i < rectChildren.Count; i++)
         {
             rowCount = i / columns;
             columnCount = i % columns;
+            float center = 0;
+            int leftOver = rectChildren.Count % columns;
+           
+            if(isCenter && (rowCount == rows - 1 && leftOver != 0)){
+                center = (parentWidth - cellSize.x * leftOver - spacing.x * (columns - 1) - padding.left - padding.right) / 2.0f; 
+            }
 
             var item = rectChildren[i];
-            var xPos = cellSize.x * columnCount + spacing.x * columnCount + padding.left;
+            var xPos = center + cellSize.x * columnCount + spacing.x * columnCount + padding.left;
+            
             var yPos = cellSize.y * rowCount + spacing.y * rowCount + padding.top;
+
+            
 
             SetChildAlongAxis(item, 0, xPos, cellSize.x);
             SetChildAlongAxis(item, 1, yPos, cellSize.y);
